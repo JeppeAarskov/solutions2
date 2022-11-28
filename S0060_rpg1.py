25fcca9a24c2191b31a73d67adc553e62148cde1
jepp8388 <jepperdebber@gmail.com>
2022-11-28 08:58:43 +0100

    x

---
+++ S0060_rpg1.py
@@ -0,0 +1,27 @@
+"""
+Exercise: Object oriented role playing game, part 1 :
+
+As always, read the whole exercise description carefully before your start to solve the exercise.
+
+Copy this file into your own solution directory. Write your solution into the copy.
+
+define a class Character with attributes name, max_health, _current_health, attackpower.
+_current_health shall be a private attribute, it is not meant to be changed from outside the class
+
+add a constructor (__init__) which accepts the classes' attributes as parameters
+add a method for printing out class objects (__repr__)
+
+add a method hit which reduces _current_health of another character by attackpower.
+example: _current_health=80 and attackpower=10: a hit reduces _current_health to 70.
+
+the method hit may not change the private attribute _current_health off a (potentially) foreign class
+for this reason we define another method get_hit which reduces _current_health of the object it belongs to by attackpower.
+
+If you get stuck, ask google, the other pupils or the teacher (in this order).
+If you have no idea how to begin, open S0061_rpg1_help.py and start from there.
+
+When your program is complete, push it to your github repository
+and compare it to the teacher's solution in S0065_rpg1_solution.py
+Then send this Teams message to your teacher: <filename> done
+Thereafter go on with the next file.
+"""
---
+++ S0067_rpg2.py
@@ -0,0 +1,31 @@
+"""
+exercise: Object oriented role playing game, part 2 :
+
+As always, read the whole exercise description carefully before your start to solve the exercise.
+
+Build on your solution of part 1.
+
+Invent two new classes, which inherit from class Character. For example Hunter and Magician.
+Your new classes shall have their own additional methods and/or attributes. Maybe they also override methods or attributes from class Character.
+
+In the main program, let objects of your new classes fight against each other until one character is dead.
+Print out what happens during the fight.
+
+In each turn, a character uses one of its capabilities (methods). Then it's the other character's turn.
+It is up to you, how your program decides in each turn, which capability to use.
+For example, the decision may be based on randomness or on a clever strategy
+
+Upgrade 1:
+Each time a character uses one of it's capabilities, add some randomness to the used power.
+
+Upgrade 2:
+Let your characters fight against each other 100 times.
+Keep track of the results.
+Try to balance your character's capabilities in such a way that each character wins about half of the fights.
+
+If you get stuck, ask google, the other pupils or the teacher (in this order).
+
+When your program is complete, push it to your github repository.
+Then send this Teams message to your teacher: <filename> done
+Thereafter go on with the next file.
+"""
