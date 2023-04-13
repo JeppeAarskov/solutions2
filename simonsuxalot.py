a955bf8e7a74aeacdbacc0540c77313186b171d0
JeppeAarskov <jepperdebber@gmail.com>
2023-04-13 09:33:15 +0200

    x

---
+++ simonsuxalot.py
@@ -0,0 +1,27 @@
+from sqlalchemy.orm import declarative_base, Session
+from sqlalchemy import Column, String, Integer
+from sqlalchemy import create_engine, select
+
+Database = 'sqlite:///../data/simon_er_usej_sql_database.db'
+Base = declarative_base()
+
+
+class Person(Base):
+    __tablename__ = "persons"
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    age = Column(Integer)
+
+
+def select_all(classparam):
+    with Session(engine) as session:
+        records = session.scalars(select(classparam))
+        result = []
+        for record in records:
+            result.append(record)
+    return result
+
+
+engine = create_engine(Database, echo=False, future=True)
+
+print(select_all(Person))
