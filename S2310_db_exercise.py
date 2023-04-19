0f97a2957674ff7bd8a32fa9b9f3e34c11423e58
JeppeAarskov <jepperdebber@gmail.com>
2023-04-19 10:39:41 +0200

    x

--- S2310_db_exercise.py
+++ S2310_db_exercise.py
@@ -33,3 +33,71 @@ Når dit program er færdigt, skal du skubbe det til dit github-repository.
 Send derefter denne Teams-besked til din lærer: <filename> færdig
 Fortsæt derefter med den næste fil.
 """
+
+from sqlalchemy.orm import declarative_base, Session
+from sqlalchemy import Column, String, Integer
+from sqlalchemy import create_engine, select
+
+Database = 'sqlite:///data/S2311_my_second_database.db'
+Base = declarative_base()
+
+
+class Customer(Base):
+    __tablename__ = "customers"
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    address = Column(Integer)
+    age = Column(Integer)
+
+    def __repr__(self):
+        return f"Customer({self.id=} {self.name=} {self.address=} {self.age=})"
+
+
+class Product(Base):
+    __tablename__ = "products"
+    id = Column(Integer, primary_key=True)
+    product_number = Column(String)
+    price = Column(Integer)
+    brand = Column(Integer)
+
+    def __repr__(self):
+        return f"Product({self.id=} {self.product_number=} {self.price=} {self.brand=})"
+
+
+def create_test_data():
+    new_items = []
+    new_items.append(Customer(name="John Hitler", address="Auschiwtz", age=30))
+    new_items.append(Customer(name="MrMasterTjop1", address="Norge", age=55))
+    new_items.append(Customer(name="Din Mor", address="Brazil", age=87))
+    new_items.append(Product(product_number="5490", price="39.99$", brand="Apple"))
+    new_items.append(Product(product_number="2349", price="99.99$", brand="Not Apple"))
+    new_items.append(Product(product_number="1515", price="69.99$", brand="Gucci"))
+    new_items.append(Product(product_number="0593", price="19.99$", brand="Din Mor"))
+    new_items.append(Product(product_number="7827", price="29.99$", brand="SharkGaming"))
+    new_items.append(Product(product_number="9427", price="79.99$", brand="Lidl"))
+    with Session(engine) as session:
+        session.add_all(new_items)
+        session.commit()
+
+
+def select_all(classparam):  # return a list of all records in classparams table
+    with Session(engine) as session:
+        records = session.scalars(select(classparam))
+        result = []
+        for record in records:
+            result.append(record)
+    return result
+
+
+def get_record(classparam, record_id):  # return the record in classparams table with a certain id   https://docs.sqlalchemy.org/en/14/tutorial/data_select.html
+    with Session(engine) as session:
+        # in the background this creates the sql query "select * from persons where id=record_id" when called with classparam=Person
+        record = session.scalars(select(classparam).where(classparam.id == record_id)).first()
+    return record
+
+
+engine = create_engine(Database, echo=False, future=True)
+Base.metadata.create_all(engine)
+
+create_test_data()
+print(get_record(Customer, 1))
